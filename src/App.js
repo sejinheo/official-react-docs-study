132d691a9c7bd1e63a067a64dd800134b1ecf399
허세진 <heosejin222@gmail.com>
2025-08-19 08:25:22 +0900

    첫 번째 컴포넌트

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Gallery from "./components/Gallery";
+import Profile from "./components/Profile";
+
+function App() {
+  return (
+    <div>
+      <Gallery />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Gallery.js
@@ -0,0 +1,14 @@
+import Profile from "./Profile";
+
+function Gallery(){
+    return(
+        <section>
+        <h1>Amazing scientists</h1>
+            <Profile />
+            <Profile />
+            <Profile />
+        </section>
+    );
+}
+
+export default Gallery;
