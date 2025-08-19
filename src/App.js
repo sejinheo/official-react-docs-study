ae7d6344ccebd56741ce643deee13ce26c47bb65
허세진 <heosejin222@gmail.com>
2025-08-19 17:12:30 +0900

    조건부 렌더링

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
-import Profile from "./components/Profile";
+function Item({ name, isPacked }) {
+  
+    return <li className="item">{isPacked ? name + ' ✅️' : name}</li>;
+  
+}
 
-function App() {
+export default function PackingList() {
   return (
-    <div>
-      <Profile />
-    </div>
+    <section>
+      <h1>Sally Ride's Packing List</h1>
+      <ul>
+        <Item
+          isPacked={true}
+          name="Space suit"
+        />
+        <Item
+          isPacked={true}
+          name="Helmet with a golden leaf"
+        />
+        <Item
+          isPacked={false}
+          name="Photo of Tam"
+        />
+      </ul>
+    </section>
   );
 }
 
-export default App;
+
