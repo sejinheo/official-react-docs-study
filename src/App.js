f8120727915ec19d871787202a2d2036eba33a77
허세진 <heosejin222@gmail.com>
2025-08-19 12:03:14 +0900

    중괄호가 있는 JSX에서 자바스크립트 사용하기

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import Gallery from "./components/Gallery";
-import Profile from "./components/Profile";
+import TodoList from "./components/TodoList";
 
 function App() {
   return (
     <div>
-      <Gallery />
+      <TodoList />
     </div>
   );
 }
--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -2,12 +2,14 @@ import Profile from "./Profile";
 
 function Gallery(){
     return(
-        <section>
+        <>
+        <h1>첫 번째 컴포넌트</h1>
+       
         <h1>Amazing scientists</h1>
             <Profile />
             <Profile />
             <Profile />
-        </section>
+        </>
     );
 }
 
